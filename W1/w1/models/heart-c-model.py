dfc068aabbc5d57423fc3ea3248b4a15a46e90c7
santidrj <santi.delrey@gmail.com>
2021-10-19 12:22:57 +0200

    Move Heart-C OPTICS to models folder
    Signed-off-by: santidrj <santi.delrey@gmail.com>

---
+++ W1/w1/models/heart-c-model.py
@@ -0,0 +1,54 @@
+import os
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt, gridspec
+from sklearn.cluster import OPTICS
+
+df_heart = pd.read_pickle(os.path.join('..', 'datasets', 'heart_normalized.pkl'))
+optics_model = OPTICS(min_samples=27)
+optics_model.fit(df_heart)
+
+# Creating a numpy array with numbers at equal spaces till
+# the specified range
+space = np.arange(len(df_heart))
+
+# Storing the reachability distance of each point
+reachability = optics_model.reachability_[optics_model.ordering_]
+
+# Storing the cluster labels of each point
+labels = optics_model.labels_[optics_model.ordering_]
+
+print(labels)
+
+# Defining the framework of the visualization
+plt.figure(figsize=(10, 7))
+G = gridspec.GridSpec(2, 1)
+ax1 = plt.subplot(G[0, :])
+ax2 = plt.subplot(G[1, 0])
+
+# Plotting the Reachability-Distance Plot
+colors = ['c.', 'b.', 'r.', 'y.', 'g.']
+for Class, colour in zip(range(0, 5), colors):
+    Xk = space[labels == Class]
+    Rk = reachability[labels == Class]
+    ax1.plot(Xk, Rk, colour, alpha=0.3)
+ax1.plot(space[labels == -1], reachability[labels == -1], 'k.', alpha=0.3)
+ax1.plot(space, np.full_like(space, 2., dtype=float), 'k-', alpha=0.5)
+ax1.plot(space, np.full_like(space, 0.5, dtype=float), 'k-.', alpha=0.5)
+ax1.set_ylabel('Reachability Distance')
+ax1.set_title('Reachability Plot')
+
+# Plotting the OPTICS Clustering
+colors = ['c.', 'b.', 'r.', 'y.', 'g.', 'm.', 'maroon']
+for Class, colour in zip(range(7), colors):
+    Xk = df_heart[optics_model.labels_ == Class]
+    ax2.plot(Xk.iloc[:, 0], Xk.iloc[:, 1], colour, alpha=0.3)
+
+ax2.plot(df_heart.iloc[optics_model.labels_ == -1, 0],
+         df_heart.iloc[optics_model.labels_ == -1, 1],
+         'k+', alpha=0.1)
+ax2.set_title('OPTICS Clustering')
+
+plt.tight_layout()
+plt.show()
--- W1/w1/processing/process_heart-c.py
+++ W1/w1/processing/process_heart-c.py
@@ -1,11 +1,8 @@
 import os.path
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
-from matplotlib import gridspec
-from sklearn.cluster import OPTICS, cluster_optics_dbscan
 from sklearn.preprocessing import RobustScaler
 
 import utils
@@ -65,54 +62,3 @@ for col in df_heart_numerical.columns:
                   color='black')
     plt.savefig(os.path.join(figs_folder_path, f'heart-c_{col}_boxplot.png'))
 
-optics_model = OPTICS(min_samples=27)
-optics_model.fit(df_heart_normalized)
-
-# Producing the labels according to the DBSCAN technique
-labels1 = cluster_optics_dbscan(reachability=optics_model.reachability_,
-                                core_distances=optics_model.core_distances_,
-                                ordering=optics_model.ordering_, eps=np.inf)
-
-# Creating a numpy array with numbers at equal spaces till
-# the specified range
-space = np.arange(len(df_heart_normalized))
-
-# Storing the reachability distance of each point
-reachability = optics_model.reachability_[optics_model.ordering_]
-
-# Storing the cluster labels of each point
-labels = optics_model.labels_[optics_model.ordering_]
-
-print(labels)
-
-# Defining the framework of the visualization
-plt.figure(figsize=(10, 7))
-G = gridspec.GridSpec(2, 1)
-ax1 = plt.subplot(G[0, :])
-ax2 = plt.subplot(G[1, 0])
-
-# Plotting the Reachability-Distance Plot
-colors = ['c.', 'b.', 'r.', 'y.', 'g.']
-for Class, colour in zip(range(0, 5), colors):
-    Xk = space[labels == Class]
-    Rk = reachability[labels == Class]
-    ax1.plot(Xk, Rk, colour, alpha=0.3)
-ax1.plot(space[labels == -1], reachability[labels == -1], 'k.', alpha=0.3)
-ax1.plot(space, np.full_like(space, 2., dtype=float), 'k-', alpha=0.5)
-ax1.plot(space, np.full_like(space, 0.5, dtype=float), 'k-.', alpha=0.5)
-ax1.set_ylabel('Reachability Distance')
-ax1.set_title('Reachability Plot')
-
-# Plotting the OPTICS Clustering
-colors = ['c.', 'b.', 'r.', 'y.', 'g.', 'm.', 'maroon']
-for Class, colour in zip(range(7), colors):
-    Xk = df_heart_normalized[optics_model.labels_ == Class]
-    ax2.plot(Xk.iloc[:, 0], Xk.iloc[:, 1], colour, alpha=0.3)
-
-ax2.plot(df_heart_normalized.iloc[optics_model.labels_ == -1, 0],
-         df_heart_normalized.iloc[optics_model.labels_ == -1, 1],
-         'k+', alpha=0.1)
-ax2.set_title('OPTICS Clustering')
-
-plt.tight_layout()
-plt.show()
