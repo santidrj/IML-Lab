045a8945c521b9c8540f917ffec1c57f9ddf6d3e
Yazmina <yazmina.zurita@gmail.com>
2021-10-30 11:43:14 +0200

    Trying Fuzzy C-means on the Connect-4 dataset.
    Signed-off-by: yazurita <yazmina.zurita@gmail.com>

--- W1/w1/models/connect4_optics.py
+++ W1/w1/models/connect4_optics.py
@@ -2,11 +2,11 @@ import os
 import pickle
 import time
 
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.cluster import OPTICS
+from matplotlib import pyplot as plt, gridspec
+import matplotlib.cm as cm
 
 import utils
 
@@ -27,6 +27,7 @@ start = time.time()
 optics_clusterings = []
 optics_clusterings.append(
     OPTICS(min_samples=min_pts, metric='l2', algorithm='kd_tree').fit(df_connect4_encoded.iloc[:, :-1]))
+"""
 optics_clusterings.append(
     OPTICS(min_samples=min_pts, metric='l2', algorithm='brute').fit(df_connect4_encoded.iloc[:, :-1]))
 optics_clusterings.append(
@@ -37,6 +38,7 @@ optics_clusterings.append(
     OPTICS(min_samples=min_pts, metric='chebyshev', algorithm='kd_tree').fit(df_connect4_encoded.iloc[:, :-1]))
 optics_clusterings.append(
     OPTICS(min_samples=min_pts, metric='chebyshev', algorithm='brute').fit(df_connect4_encoded.iloc[:, :-1]))
+"""
 end = time.time()
 
 with open('connect4_optics_metrics', 'wb') as file:
@@ -55,31 +57,45 @@ def optics_plots(df, models):
     labels = models.labels_
     print(labels)
     label_set = set(labels)
-    colors = cm.rainbow(np.linspace(0, 1, len(label_set)))
+    colors = colors = ['c.', 'b.', 'r.', 'y.', 'g.']
     reachability = models.reachability_[models.ordering_]
+    unique, counts = np.unique(labels, return_counts=True)
+
+    plt.figure(figsize=(10, 10))
+    g = gridspec.GridSpec(2, 1)
+    ax1 = plt.subplot(g[0, :])
+    ax2 = plt.subplot(g[1, :])
 
     for l, c in zip(label_set, colors):
-        # plt.scatter(df['a3'][labels == l], df['a4'][labels == l], color=c)
         if l == -1:
-            plt.plot(reachability[labels == l], color='k', marker='.', ls='')
+            ax1.plot(reachability[labels == l], color='k', marker='.', ls='', alpha=0.3)
+            x, y = df['a3'][labels == l], df['a4'][labels == l]
+            ax2.scatter(x, y, color='k', alpha=0.3)
+            ax2.text(x * (1 + 0.01), y * (1 + 0.01), counts[unique == l], fontsize=12)
         else:
-            plt.plot(reachability[labels == l], color=c, marker='.', ls='')
+            ax1.plot(reachability[labels == l], color=c, marker='.', ls='', alpha=0.3)
+            ax2.scatter(df['a3'][labels == l], df['a4'][labels == l], color=c, alpha=0.3)
+
     plt.show()
 
 
-# optics_plots(df_connect4_encoded, optics_clusterings)
+#optics_plots(df_connect4_encoded, optics_clusterings)
 
 path_val = os.path.join('..', '..', 'validation', 'connect4_val.txt')
 for i, m in enumerate(metrics):
     for j, a in enumerate(algorithms):
         optics_model = optics_clusterings[i + j]
+        unique, counts = np.unique(optics_model.labels_, return_counts=True)
 
         with open(path_val, 'a') as f:
-            f.write(
-                f'\n \n*OPTICS: min_pts = {min_pts}, eps = {max_eps}, method =, {cluster_mth}  metric = {m}, algorithm = {a}')
+            f.write(f'\n \n*OPTICS: min_pts = {min_pts}, eps = {max_eps}, method =, {cluster_mth}, unique = {unique}, counts = {counts}  metric = {m}, algorithm = {a}')
 
-        utils.print_metrics(df_connect4_encoded.iloc[:, :-1], df_connect4_encoded['class'], optics_model.labels_,
-                            file_path=path_val, isOPTICS=True)
+        utils.print_metrics(df_connect4_encoded.iloc[:, :-1], df_connect4_encoded['class'], optics_model.labels_, file_path=path_val, isOPTICS=True)
+
+        """
+        plt.bar(unique, counts)
+        """
+
 
 """
 unique, counts = np.unique(optics_clusters.labels_, return_counts=True)
