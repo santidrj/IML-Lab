9c87891396ff6d1e00a0f448af43986804aa2d59
santidrj <santi.delrey@gmail.com>
2021-11-07 21:36:06 +0100

    Create UMAP 3D plot

--- W2/w2/models/heartc_umap.py
+++ W2/w2/models/heartc_umap.py
@@ -9,14 +9,19 @@ data_root_path = os.path.join('..', '..', 'datasets')
 df = pd.read_pickle(os.path.join(data_root_path, 'processed', 'processed_heart-c.pkl'))
 df_gs = pd.read_pickle(os.path.join(data_root_path, 'processed', 'heart-c_gs.pkl'))
 
-reducer = umap.UMAP()
+reducer = umap.UMAP(n_components=3)
 reducer.fit(df)
 embedding = reducer.embedding_
 
 print(embedding.shape)
 
-sns.set(style='white', context='notebook', rc={'figure.figsize': (14, 10)})
-plt.scatter(embedding[:, 0], embedding[:, 1], c=[sns.color_palette()[x] for x in df_gs.map({'<50': 0, '>50_1': 1})])
-plt.gca().set_aspect('equal', 'datalim')
-plt.title('UMAP projection of the Heart-C dataset', fontsize=24)
+# sns.set(style='white', context='paper', rc={'figure.figsize': (14, 10)})
+# plt.scatter(embedding[:, 0], embedding[:, 1], c=[sns.color_palette()[x] for x in df_gs.map({'<50': 0, '>50_1': 1})])
+# plt.gca().set_aspect('equal', 'datalim')
+# plt.title('UMAP projection of the Heart-C dataset', fontsize=24)
+# plt.show()
+
+ax = plt.subplot(projection='3d')
+ax.scatter(embedding[:, 0], embedding[:, 1], embedding[:, 2],
+           c=[sns.color_palette()[x] for x in df_gs.map({'<50': 0, '>50_1': 1})], s=0.1, alpha=0.9)
 plt.show()
