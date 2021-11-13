1e0217399f0da3ab4e611a5c63dd82f6214fdef3
santidrj <santi.delrey@gmail.com>
2021-11-13 20:53:46 +0100

    Rename PCA.py to pca.py

--- W2/w2/models/connect4_reduction.py
+++ W2/w2/models/connect4_reduction.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA, IncrementalPCA
 import umap
 
-from algorithms.PCA import PCA as OurPCA
+from algorithms.pca import PCA as OurPCA
 
 path_data = os.path.join('..', '..', 'datasets', 'processed')
 path_models = os.path.join('..', '..', 'models_results', 'connect-4')
