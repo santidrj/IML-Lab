a561905784034c85c418981760cfc1985afcbe11
Yazmina <yazmina.zurita@gmail.com>
2021-11-20 21:11:38 +0100

    Run with final configurations

--- W2/w2/models/connect4_kmeans.py
+++ W2/w2/models/connect4_kmeans.py
@@ -21,16 +21,20 @@ init_method = 'k-means++'
 metric = 'euclidean'
 n_iter = 300
 init = 10
+nc = 16
+n_comp = 16
+n_nb = 30
 
 print("Starting K-Means in Connect-4")
 
 df = pd.read_pickle(os.path.join(path_data, 'connect4_encoded.pkl'))
 true_class = df[df.columns[-1]]
 df = df.drop(columns=df.columns[-1])
-"""
-df_custom_pca = pd.read_pickle(os.path.join(path_data, 'connect4_custom-pca-4.pkl'))
 
-#with open(os.path.join(path_data, f'connect4_umap-30-10.pkl'), 'rb') as f:
+with open(os.path.join(path_data, f'connect4_custom-pca-{nc}.pkl'), 'rb') as f:
+    df_custom_pca = pd.DataFrame(pickle.load(f))
+
+with open(os.path.join(path_data, f'connect4_umap-{n_nb}-{n_comp}.pkl'), 'rb') as f:
     df_umap = pd.DataFrame(pickle.load(f))
 
 ##### K-MEANS WITH ORIGINAL DATASET #####
@@ -45,6 +49,7 @@ with open(os.path.join(path_models, f'k-means.pkl'), 'wb') as f:
 with open(os.path.join(path_models, f'k-means.pkl'), 'rb') as f:
     kmeans = pickle.load(f)
 
+
 ##### K-MENAS WITH CUSTOM PCA REDUCED DATASET #####
 start_kmeans_pca = time.time()
 kmeans_pca = Kmeans(k=K, init=init_method, metric=metric, max_iter=n_iter, n_init=init)
@@ -58,30 +63,35 @@ with open(os.path.join(path_models, f'k-means-pca.pkl'), 'rb') as f:
     kmeans_pca = pickle.load(f)
 
 
+"""
 ##### K-MENAS WITH UMAP REDUCED DATASET #####
 start_kmeans_umap = time.time()
 kmeans_umap = Kmeans(k=K, init=init_method, metric=metric, max_iter=n_iter, n_init=init)
 kmeans_umap.fit(df_umap)
 end_kmeans_umap = time.time()
 
-with open(os.path.join(path_models, f'k-means-umap-{K}-{init_method}-{metric}.pkl'), 'wb') as f:
+with open(os.path.join(path_models, f'k-means-umap.pkl'), 'wb') as f:
     pickle.dump(kmeans_umap, f)
-
-#with open(os.path.join(path_models, f'k-means-umap-{K}-{init_method}-{metric}.pkl'), 'rb') as f:
+"""
+with open(os.path.join(path_models, f'k-means-umap.pkl'), 'rb') as f:
     kmeans_umap = pickle.load(f)
+
 
 models = [kmeans, kmeans_pca, kmeans_umap]
 models_names = ['k-means', 'k-means-pca', 'k-means-umap']
-models_times = [end_kmeans-start_kmeans, end_kmeans_pca-start_kmeans_pca, end_kmeans_umap-start_kmeans_umap]
+models_times = [end_kmeans-start_kmeans, end_kmeans_pca-start_kmeans_pca, 615.3271589279175]
 
 for model, name, time in zip(models, models_names, models_times):
-    print(f'Sum of squared error: {model.square_error}')
+    #print(f'Sum of squared error: {model.square_error}')
+
+    labels = model.labels
+    unique, counts = np.unique(labels, return_counts=True)
+    counts = counts / len(labels)
     
     plt.figure(figsize=(6, 5))
     ax = plt.subplot(111)
-    
     colors = ['c', 'b', 'r', 'y', 'g', 'm', 'maroon', 'crimson', 'darkgreen', 'peru']
-    labels = model.labels
+
     for Class, colour in zip(range(9), colors):
         Xk = df[labels == Class]
         ax.plot(Xk.iloc[:, 6], Xk.iloc[:, 7], 'o', color=colour, alpha=0.3)
@@ -94,30 +104,39 @@ for model, name, time in zip(models, models_names, models_times):
     
     plt.savefig(os.path.join('..', '..', 'figures', 'connect-4', f'connect-4_{name}-{K}-{init_method}-{metric}.png'))
     plt.show()
-
+    
     with open(os.path.join(path_val, 'connect-4_k-means_val.txt'), 'a') as f:
         f.write(f'\n \n{name}: K = {K}, init = {init_method}, metric = {metric}, max_inter = {n_iter}, n_init = {init}')
+        f.write(f'pca components = {nc}, n_neighbors = {n_nb}, n_components = {n_comp}')
+        f.write(f'\nPercentage of samples in each cluster: {counts}')
         f.write(f'\nExecution time: {time} s')
     
-    utils.print_metrics(df, true_class, labels, path_val)
-"""
+    utils.print_metrics(df, true_class, labels, os.path.join(path_val, 'connect-4_k-means_val.txt'))
 
-for n_comp in [10, 20, 30]:
-    for n_nb in [30, 60, 90]:
+
+"""
+for n_comp in [2]:
+    for n_nb in [30]:
         with open(os.path.join(path_data, f'connect4_umap-{n_nb}-{n_comp}.pkl'), 'rb') as f:
             df_umap = pd.DataFrame(pickle.load(f))
-
+        start_kmeans_umap = time.time()
         kmeans_umap = Kmeans(k=K, init=init_method, metric=metric, max_iter=n_iter, n_init=init)
         kmeans_umap.fit(df_umap)
+        end_kmeans_umap = time.time()
+        labels = kmeans_umap.labels
+        unique, counts = np.unique(labels, return_counts=True)
+        counts = counts / len(labels)
 
         with open(os.path.join(path_models, f'k-means-umap-{n_nb}-{n_comp}.pkl'), 'wb') as f:
             pickle.dump(kmeans_umap, f)
 
         with open(os.path.join(path_val, 'connect-4_k-means_val.txt'), 'a') as f:
-            f.write(
-                f'\n \nUMAP: n_neighbors = {n_nb}, n_components = {n_comp}, min_dist = 0')
+            f.write(f'\n \nUMAP: n_neighbors = {n_nb}, n_components = {n_comp}, min_dist = 0')
+            f.write(f'pca components = {nc}, n_neighbors = {n_nb}, n_components = {n_comp}')
+            f.write(f'\nPercentage of samples in each cluster: {counts}')
+            f.write(f'\nExecution time: {end_kmeans_umap-start_kmeans_umap} s')
 
         utils.print_metrics(df, true_class, kmeans_umap.labels, os.path.join(path_val, 'connect-4_k-means_val.txt'))
 
-
+"""
 print("Finished K-Means in Connect-4")
