f9bcff2ffc03c6a7e02acf9b1ea505624dfc5b7f
Santiago Del Rey <59149491+santidrj@users.noreply.github.com>
2021-12-12 21:17:03 +0100

    Refactor IBL.py

--- W3/w3/algorithms/IBL.py
+++ W3/w3/algorithms/IBL.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io import arff
+from sklearn.preprocessing import RobustScaler
+
 
 def similarity(x, y):
     """
@@ -13,7 +15,35 @@ def similarity(x, y):
     return:
       the similarity distance between x and y
     """
+
     return np.sqrt(np.square(x - y).sum())
+
+
+def preprocess(data: DataFrame):
+    """
+    Splits the data into numerical and categorical variables.
+    It also normalizes the numerical variables.
+
+    Parameters
+    ----------
+    data: DataFrame
+        Dataset to be preprocessed.
+
+    Returns
+    -------
+    tuple
+        a tuple with the normalized numerical features as the first element 
+        and the categorical features as the second element.
+    """
+
+    numeric_features = data.select_dtypes(include="number")
+    normalized_num_features = RobustScaler(unit_variance=True).fit_transform(
+        numeric_features
+    )
+
+    categorical_features = data.select_dtypes(include="object")
+
+    return normalized_num_features, categorical_features
 
 
 """
@@ -40,13 +70,14 @@ class IBL:
 
         if the value is not pass, IBL1 is selected.
     """
-    def __init__(self, dataframe, algorithm='ibl1'):
+
+    def __init__(self, dataframe: Dataframe, algorithm="ibl1"):
         self.training_set = dataframe
         self.number_samples = len(dataframe)
         self.algorithm = algorithm
         self.correct_samples = 0
         self.incorrect_samples = 0
-        self.accuracy = None
+        self.accuracy = 0
         self.saved_samples = 0
         self.execution_time = 0
         self.cd = set()
@@ -71,7 +102,7 @@ class IBL:
     def _ibl2(self):
         for i in range(self.number_samples):
             x = self.training_set.iloc[i]
-            if not cd:
+            if not self.cd:
                 self.cd.add(x)
             else:
                 similarity_list = [similarity(x[:-1], y[:-1]) for y in cd]
@@ -87,29 +118,31 @@ class IBL:
             x = self.training_set.iloc[i]
 
     def _run(self):
-        if self.algorihtm in {'ibl1', 'ibl2', 'ibl3'}:
-            if self.algorithm is 'ibl1':
+        # TODO: Implement below
+        # self.training_set = preprocess(self.training_set)
+        if self.algorihtm in {"ibl1", "ibl2", "ibl3"}:
+            if self.algorithm is "ibl1":
                 start = time.time()
                 self._ibl1()
                 end = time.time()
                 self.execution_time = end - start
-            elif self.algorithm is 'ibl2':
+            elif self.algorithm is "ibl2":
                 start = time.time()
                 self._ibl2()
                 end = time.time()
                 self.execution_time = end - start
-            elif self.algorithm is 'ibl3':
+            elif self.algorithm is "ibl3":
                 start = time.time()
                 self._ibl3()
                 end = time.time()
                 self.execution_time = end - start
 
-            self.accuracy = str(self.correct_samples / self.number_samples) + '%'
+            self.accuracy = self.correct_samples / self.number_samples
         else:
-            print("You selected a wrong Instance based learning algorithm")
+            print("You selected a wrong Instance-Based Learning algorithm")
 
     def print_results(self):
-        print("Performance metrics of the", self.algorithm, " algorithm")
-        print("Number of saved instances: ", self.saved_samples)
-        print("Execution time: ", self.execution_time)
-        print("Accuracy: " + self.accuracy)
+        print(f"Performance metrics of the {self.algorithm} algorithm")
+        print(f"Number of saved instances: {self.saved_samples}")
+        print(f"Execution time: {self.execution_time}")
+        print(f"Accuracy: {self.accuracy}%")
