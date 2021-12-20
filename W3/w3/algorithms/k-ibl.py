c28fc4598fbe03e12ca5009106b3d325f3c96480
Yazmina <yazmina.zurita@gmail.com>
2021-12-20 09:15:56 +0100

    Simplify code of the Modified Plurality policy.

--- W3/w3/algorithms/k-ibl.py
+++ W3/w3/algorithms/k-ibl.py
@@ -1,24 +1,46 @@
 import numpy as np
 
-# y_nearest = sorted(np.sum(similarity_list[:-1], axis=1))[:k]
 
+def vote(votes, policy='most_voted', mp_k=1):
+    """
+    Computes the class of a sample according to
+    the classes of its k neighbours
 
-def vote(y_nearest, policy='most_voted', mp_k=1):
-    votes = [y[-1] for y in y_nearest]
+    :param votes: classes of the k nearest neighbours
+        ---REMOVE---
+        y_nearest = sorted(np.sum(similarity_list[:-1], axis=1))[:k]
+        votes = [y[-1] for y in y_nearest]
+        ---REMOVE---
+    :param policy: Most Voted Solution, Modified Plurality or Borda Count
+    :param mp_k: number of neighbours to remove in case of tie in the Modified Plurality policy
+    :return: winner class
+    """
 
     if policy == 'most_voted':
-        return max(votes, key = votes.count)
+        """
+        ---REMOVE---
+        Note to my dear partners:
+        In case of tie among classes, this policy returns the first occurrence 
+        of those classes in "votes", which corresponds to the nearest 
+        neighbour of that class because "votes" is sorted according to distance to
+        the sample
+        ---REMOVE---
+        """
+        return max(votes, key=votes.count)
 
     if policy == 'mod_plurality':
-        unique, counts = np.unique(votes, return_counts=True)
-        # Sort unique values by their frequency
-        counts_sort_ind = np.argsort(-counts)
-        unique = unique[counts_sort_ind]
-        counts = counts[counts_sort_ind]
+        # Unique options sorted by decreasing number of votes
+        options_srt = sorted(set(votes), key=votes.count, reverse=True)
+        # Votes for each option
+        count_srt = [votes.count(x) for x in options_srt]
 
-        if len(counts) > 1 and counts[0] == counts[1]:
-            return vote(y_nearest[:mp_k], policy='mod_plurality', mp_k=mp_k)
+        # In case of tie, remove mp_k neighbours and repeat
+        if len(count_srt) > 1 and count_srt[0] == count_srt[1]:
+            return vote(votes[:mp_k], policy='mod_plurality', mp_k=mp_k)
         else:
-            return unique[0]
+            return options_srt[0]
+
+
+
 
 
