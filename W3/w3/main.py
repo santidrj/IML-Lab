dfa3d1f360dea20f112203edc57f3ac8febd9b2a
Santiago del Rey <59149491+santidrj@users.noreply.github.com>
2021-12-24 18:05:21 +0100

    Refactor main.py

--- W3/w3/main.py
+++ W3/w3/main.py
@@ -3,12 +3,11 @@ import os
 from models.ibl_eval import IBLEval
 
 data_root_path = os.path.join('..', 'datasets', 'datasets')
-# ibl_eval = IBLEval(os.path.join(data_root_path, 'pen-based'))
-ibl_eval = IBLEval(os.path.join(data_root_path, 'labor'))
-ibl_eval.run(algorithms=['k-ibl'])
-# ibl_eval.write_results(os.path.join('..', 'results', 'pen-based-results.txt'), algorithms=['ibl1', 'ibl2'])
-ibl_eval.write_results(os.path.join('..', 'results', 'labor-results.txt'), algorithms=['k-ibl'])
-ibl_eval.print_results(algorithms=['k-ibl'])
-# accuracy, time = ibl_eval.feed_data('autos.fold.000000.train.arff', 'autos.fold.000000.test.arff')
-# print(accuracy)
-# print(time)
+results_path = os.path.join('..', 'results')
+
+dataset = 'sick'
+ibl_eval = IBLEval(os.path.join(data_root_path, dataset))
+algorithms = ['k-ibl']
+ibl_eval.run(algorithms=algorithms)
+ibl_eval.write_results(os.path.join(results_path, f'{dataset}-results.txt'), algorithms=algorithms)
+ibl_eval.print_results(algorithms=algorithms)
