46648405908bdadb003fbf6d8bf4fa78f0acc7ce
tarmac <aug@tarmac.com>
2021-12-28 10:36:01 +0100

    create selectionkIBLAlgorithm.py and divide the work into subtask

---
+++ W3/w3/algorithms/selectionkIBLAlgorithm.py
@@ -0,0 +1,13 @@
+"""
+- Modify the distance metric in the k-IBl so that it used preprocess features weights
+- We may choose 2 algorithms (filter or wrapper)
+- the selection will be computed in the training set, as preprocessing step.
+- some examplesReliefF, Information Gain, or the Correlation
+
+- analyze the results of selectionkIBLAlgorithm vs kIBLAlgorithm
+    - set both algorithm with the best combination and evaluate the classification accuracy
+"""
+
+
+class selectionkIBLAlgorithm:
+    pass
